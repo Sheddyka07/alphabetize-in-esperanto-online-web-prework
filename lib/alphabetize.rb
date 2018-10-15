fbfee7f71e8b696627eabcc826995ae4844c6bf1
Kelly Sheddy <jungaka07@gmail.com>
2018-10-15 14:52:49 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by |sentence|
+  arr_sort_by |sentence|
     sentence.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
     end 

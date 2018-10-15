0d98863c6ebb1bc27c68cbb83d5eaf065de6923d
Kelly Sheddy <jungaka07@gmail.com>
2018-10-15 14:40:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,3 @@
 def alphabetize(arr)
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
- a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
-word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
-word_hash = set_word_hash_values(a_to_z_hash, word_hash) #sets values of word hash to array of #'s'
-words = sort_words(word_hash) #sorts words
-
   arr.sort_by {|word| word}
 end
